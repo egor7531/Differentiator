b96501b01825678467eaba12716e8fb202d4f6e1
egor7531 <egorkokonin557@gmail.com>
2023-12-15 16:02:57 +0300

    add derivative without variable

--- Differentiator.cpp
+++ Differentiator.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 #include "Tree.h"
 #include "TreeDump.h"
@@ -14,19 +15,25 @@ enum Operators
     OP_SUB,
     OP_MUL,
     OP_DIV,
-    OP_POW
+    OP_POW,
+    OP_SQRT,
+    OP_SIN,
+    OP_COS,
+    NO_OP
 };
 
 enum TypeElem
 {
     OPERATOR,
-    NUM
+    NUM,
+    VARIABLE
 };
 
 union Data
 {
     double      value;
     Operators   op;
+    char*       variable;
 };
 
 struct NodeData
@@ -43,26 +50,35 @@ void write_expression_in_tree(Tree* tree, TreeNode **node, char **buf);
 Operators define_operator(char* command);
 NodeData* get_object(char** buf);
 double calculate_expression(TreeNode* node);
+void optimize_derivative(TreeNode** node);
+void get_derivative(Tree* tree, TreeNode* node);
+void print_derivative(const char* nameFile, const Tree* tree);
+void print_nodes(FILE* fp, const TreeNode* node);
 
-double pow(double x, int n);
+const char* nameFileTxtEx  = "Expression.txt";
+const char* nameFileDotEx  = "Expression.dot";
+const char* nameFilePngEx  = "Expression.png";
 
-const char* nameFile = "Expression.txt";
-const char* nameFileDot = "Dump.dot";
-const char* nameFilePng = "Dump.png";
+const char* nameFileTxtDer = "Derivative.txt";
+const char* nameFileDotDer = "Derivative.dot";
+const char* nameFilePngDer = "Derivative.png";
 
 const int MAX_SIZE_OBJECT = 50;
 
 int main()
 {
-    char* buf = get_file_content(nameFile);
-    Tree* tree = tree_ctor(elem_ctor, elem_dtor, write_elem);
+    char* buf = get_file_content(nameFileTxtEx);
+    Tree* expression = tree_ctor(elem_ctor, elem_dtor, write_elem);
 
-    write_expression_in_tree(tree, &tree->root, &buf);
-    tree_graphic_dump(tree, nameFileDot, nameFilePng);
+    write_expression_in_tree(expression, &expression->root, &buf);
+    tree_graphic_dump(expression,  nameFileDotEx,  nameFilePngEx);
 
-    printf("%lf", calculate_expression(tree->root));
+    get_derivative(expression, expression->root);
+    tree_graphic_dump(expression,  nameFileDotDer,  nameFilePngDer);
 
-    tree_dtor(tree);
+    print_derivative(nameFileTxtDer, expression);
+
+    tree_dtor(expression);
     return 0;
 }
 
@@ -79,10 +95,15 @@ void* elem_ctor(void* elem)
         data->type = OPERATOR;
         data->elem.op = ((NodeData*)elem)->elem.op;
     }
-    else
+    else if(((NodeData*)elem)->type == NUM)
     {
         data->type = NUM;
         data->elem.value = ((NodeData*)elem)->elem.value;
+    }
+    else
+    {
+        data->type = VARIABLE;
+        data->elem.variable = strdup(((NodeData*)elem)->elem.variable);
     }
 
     return data;
@@ -90,16 +111,21 @@ void* elem_ctor(void* elem)
 
 void elem_dtor(void* elem)
 {
+    assert(elem != nullptr);
+
+    free(((NodeData*)elem)->elem.variable);
     free(elem);
 }
 
-void  write_elem(FILE* fp, void* elem)
+void write_elem(FILE* fp, void* elem)
 {
     assert(fp != nullptr);
     assert(elem != nullptr);
 
     if(((NodeData*)elem)->type == NUM)
         fprintf(fp, "%.2lf", ((NodeData*)elem)->elem.value);
+    else if(((NodeData*)elem)->type == VARIABLE)
+        fprintf(fp, "%s", ((NodeData*)elem)->elem.variable);
     else
     {
         switch(((NodeData*)elem)->elem.op)
@@ -118,6 +144,15 @@ void  write_elem(FILE* fp, void* elem)
                 break;
             case OP_POW:
                 fprintf(fp, "%c", '^');
+                break;
+            case OP_SQRT:
+                fprintf(fp, "%s", "sqrt");
+                break;
+            case OP_SIN:
+                fprintf(fp, "%s", "sin");
+                break;
+            case OP_COS:
+                fprintf(fp, "%s", "cos");
                 break;
             default:
                 assert("Unknown operator");
@@ -176,6 +211,14 @@ Operators define_operator(char* command)
         return OP_MUL;
     if(!strcmp(command, "pow"))
         return OP_POW;
+    if(!strcmp(command, "sqrt"))
+        return OP_SQRT;
+    if(!strcmp(command, "sin"))
+        return OP_SIN;
+    if(!strcmp(command, "cos"))
+        return OP_COS;
+
+    return NO_OP;
 }
 
 NodeData* get_object(char** buf)
@@ -197,6 +240,11 @@ NodeData* get_object(char** buf)
         data->elem.value = atof(object);
         data->type = NUM;
     }
+    else if(!strcmp(object, "x"))
+    {
+        data->elem.variable = strdup(object);
+        data->type = VARIABLE;
+    }
     else
     {
         data->elem.op = define_operator(object);
@@ -212,7 +260,7 @@ NodeData* get_object(char** buf)
 double calculate_expression(TreeNode* node)
 {
     if(node == nullptr)
-        return 0;
+        return NAN;
 
     if(((NodeData*)(node->elem))->type == NUM)
         return ((NodeData*)(node->elem))->elem.value;
@@ -232,19 +280,110 @@ double calculate_expression(TreeNode* node)
             return valueLeft * valueRight;
         case OP_POW:
             return pow(valueLeft, valueRight);
+        case OP_SQRT:
+            return sqrt(valueLeft);
+        case OP_SIN:
+            return sin(valueLeft);
+        case OP_COS:
+            return cos(valueLeft);
         default:
             assert("Unknown operator");
     }
 
-    return -1;
+    return NAN;
 }
 
-double pow(double x, int n)
+/*void optimize_derivative(TreeNode** node)
 {
-    assert(n < 0);
+    if(*node == nullptr)
+        return;
 
-    if(n == 1)
-        return 1;
+    if( ((NodeData*)((*node)->elem))->type == OPERATOR)
+    {
+        if( ((NodeData*)((*node)->leftNode->elem))->type == NUM &&
+            ((NodeData*)((*node)->rightNode->elem))->type == NUM)
+        {
+            ((NodeData*)((*node)->elem))->type = NUM;
 
-    return x * pow(x, n - 1);
+            double valueLeft = ((NodeData*)((*node)->leftNode->elem))->elem.value;
+            double valueRight = ((NodeData*)((*node)->rightNode->elem))->elem.value;
+
+            switch(((NodeData*)((*node)->elem))->elem.op)
+            {
+                case OP_ADD:
+                    ((NodeData*)((*node)->elem))->elem.value = valueLeft + valueRight;
+                    break;
+                case OP_SUB:
+                    ((NodeData*)((*node)->elem))->elem.value = valueLeft - valueRight;
+                    break;
+                case OP_DIV:
+                    ((NodeData*)((*node)->elem))->elem.value = valueLeft / valueRight;
+                    break;
+                case OP_MUL:
+                    ((NodeData*)((*node)->elem))->elem.value = valueLeft * valueRight;
+                    break;
+                case OP_POW:
+                    ((NodeData*)((*node)->elem))->elem.value = pow(valueLeft, valueRight);
+                    break;
+                default:
+                    assert("Unknown operator");
+            }
+
+            elem_dtor(((*node)->leftNode->elem));
+            elem_dtor(((*node)->rightNode->elem));
+            free(((*node)->leftNode));
+            free(((*node)->rightNode));
+            (*node)->leftNode = nullptr;
+            (*node)->rightNode = nullptr;
+        }
+}*/
+
+void get_derivative(Tree* tree, TreeNode* node)
+{
+    if(((NodeData*)(node->elem))->type == NUM)
+        ((NodeData*)(node->elem))->elem.value = 0;
+    else
+    {
+        switch(((NodeData*)(node->elem))->elem.op)
+        {
+            case OP_ADD:
+                get_derivative(tree, node->leftNode);
+                get_derivative(tree, node->rightNode);
+                break;
+            case OP_SUB:
+                get_derivative(tree, node->leftNode);
+                get_derivative(tree, node->rightNode);
+                break;
+            default:
+                assert("Unknown operator");
+                break;
+        }
+    }
+}
+
+void print_derivative(const char* nameFile, const Tree* tree)
+{
+    assert(nameFile != nullptr);
+    assert(tree != nullptr);
+
+    FILE* fp = fopen(nameFile, "wb");
+    if(fp == nullptr)
+        return;
+
+    print_nodes(fp, tree->root);
+    fclose(fp);
+}
+
+void print_nodes(FILE* fp, const TreeNode* node)
+{
+    assert(fp != nullptr);
+
+    if(node == nullptr)
+        return;
+    fprintf(fp, "( ");
+    print_nodes(fp, node->leftNode);
+    write_elem(fp, node->elem);
+    fprintf(fp, " ");
+    print_nodes(fp, node->rightNode);
+    fprintf(fp, ") ");
 }
